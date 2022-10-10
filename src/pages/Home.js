a18add949eae398250034844c5898a43a2efb3df
Anna Tasheva <anna.tasheva90@gmail.com>
2022-10-10 23:25:47 +0100

    Updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
                     <img src={AnnasPic} alt="Anna's photo" />
                 </div>
                 <div><h2>Hi! <span><img src={Wave} className="wave" /></span></h2></div>
-                <h2>I'm Anna, a web developer based in the UK.</h2>
+                <h2>I'm Anna, a web developer based in London.</h2>
                 <p className='prompt'>I enjoy design and problem solving.The technologies I use include HTML5, CSS3, JavaScript, React, Redux. See below the projects I did while learning to code.</p>
             </div>
 
