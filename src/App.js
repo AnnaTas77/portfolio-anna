06b2dd84bdfdfaad5c9d5e63e1f04afd1fc43b66
Anna Tasheva <anna.tasheva90@gmail.com>
2022-10-19 10:02:45 +0100

    Buttons design update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import NavBar from './components/NavBar'
 import Home from "./pages/Home"
 import Projects from "./pages/Projects"
 import Contact from "./pages/Contact"
-import NavBar from './components/NavBar'
 import Footer from "./components/Footer"
+
 import './index.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">.
       <NavBar />
       <Home />
       <Projects />
