59b10cb317b25ac1bc442cd28e45801b97aad2d0
Anna Tasheva <anna.tasheva90@gmail.com>
2022-11-22 15:38:59 +0000

    Home section - about me description updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
                 </div>
                 <div><h2>Hi! <span><img src={Wave} className="wave" /></span></h2></div>
                 <h2>I'm Anna Tasheva, a junior front-end developer based in London.</h2>
-                <p className='prompt'>I have been programming for a year and have experience with JavaScript (ES6), React, Redux, HTML5 and CSS. Please see below some of my coding projects. </p>
+                <p className='prompt'> Passionate about design and programming and currently working on my career change to a front-end developer. I have experience with JavaScript (ES6), React, Redux, HTML5 and CSS. Please see below some of my coding projects. </p>
             </div>
 
         </div>
