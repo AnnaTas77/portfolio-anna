1543cf87f77d16069da9790c8221c8ca7a34e30d
Anna Tasheva <anna.tasheva90@gmail.com>
2023-07-31 20:50:55 +0100

    added new project cards and small refactoring

---
+++ src/components/ProjectItem.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import NewTabIcon from "../assets/new-window-white.svg";
+
+const ProjectItem = ({ image, name, description, live, github }) => {
+    const openUrl = (url) => {
+        return window.open(url, "_blank");
+    };
+
+    return (
+        <div className="project-container">
+            <div className="project-item">
+                <img src={image} className="project-image"></img>
+
+                <h1 className="name">{name}</h1>
+                <div className="project-description">
+                    {description}
+                    <div className="project-socials">
+                        {live ? (
+                            <button className="live-link" onClick={() => openUrl(live)}>
+                                <a href={live} aria-label="Live" target="_blank">
+                                    <img src={NewTabIcon} />
+                                    <span className="pulsate">Live</span>
+                                </a>
+                            </button>
+                        ) : (
+                            ""
+                        )}
+                        {github.map((githubLink) => {
+                            return (
+                                <button className="github-code" onClick={() => openUrl(githubLink.url ? githubLink.url : githubLink)}>
+                                    <a href={githubLink.url ? githubLink.url : githubLink} aria-label="Code" target="_blank">
+                                        <img src={NewTabIcon} />
+                                        <span className="pulsate">{githubLink.label ? githubLink.label : "Code"}</span>
+                                    </a>
+                                </button>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProjectItem;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,44 +1,52 @@
-import React from 'react'
-import '../styles/projects.css'
-import ProjectItem from '../components/ProjectItem'
-import RedditGeeks from '../assets/RedditScreenshot7.jpg'
-import WeatherApp from '../assets/WeatherApp_merged7.jpg'
-import OnlineStoreCart from '../assets/Anna_Store_Best.jpg'
-
-
+import React from "react";
+import "../styles/projects.css";
+import ProjectItem from "../components/ProjectItem";
+import RedditGeeks from "../assets/RedditScreenshot7.jpg";
+import WeatherApp from "../assets/WeatherApp_merged7.jpg";
+import NcNews from "../assets/NcNews.png";
+import Meteorites from "../assets/Meteorite Landings Viewer.png";
 
 const Projects = () => {
-
     return (
-        <div className='projects' id="projects">
-            <h1 className='title'>Personal Projects</h1>
-            <div className='project-list'>
+        <div className="projects" id="projects">
+            <h1 className="title">Personal Projects</h1>
+            <div className="project-list">
+                <ProjectItem
+                    name="NC News"
+                    image={NcNews}
+                    description="This application is designed to provide users with access to news articles, comments, topics, and user information through an intuitive and user-friendly interface."
+                    live="https://nc-news-anna.netlify.app/"
+                    github={[
+                        { label: "Frontend Code", url: "https://github.com/AnnaTas77/nc-news-frontend-project" },
+                        { label: "Backend Code", url: "https://github.com/AnnaTas77/nc-news-backend-project" },
+                    ]}
+                />
 
                 <ProjectItem
-                    name='Reddit Geeks'
+                    name="Meteorite Landings Viewer"
+                    image={Meteorites}
+                    description="Visualizer of meteorite landings sites. Data is fetched from NASA API and sorted/displayed in a user friendly manner. "
+                    github={["https://github.com/AnnaTas77/fe-react-data-visualisation"]}
+                />
+
+                <ProjectItem
+                    name="Reddit Geeks"
                     image={RedditGeeks}
-                    description='Reddit Geeks is a portfolio project for a subreddit feed app which can search through subreddits and display their articles.'
-                    live='https://annatas77.github.io/redditgeeks/'
-                    github='https://github.com/AnnaTas77/redditgeeks'
+                    description="Reddit Geeks is a portfolio project for a subreddit feed app which can search through subreddits and display their articles."
+                    live="https://annatas77.github.io/redditgeeks/"
+                    github={["https://github.com/AnnaTas77/redditgeeks"]}
                 />
 
                 <ProjectItem
-                    name='Weather App'
+                    name="Weather App"
                     image={WeatherApp}
-                    description='A simple weather app created with JavaScript, CSS and HTML. The app returns current weather data from the Open Weather Map API via Fetch API.'
-                    live='https://annatas-weather-app.netlify.app/'
-                    github='https://github.com/AnnaTas77/weather-app'
-                />
-
-                <ProjectItem
-                    name='Shopping Cart'
-                    image={OnlineStoreCart}
-                    description='An online shopping cart created with React and Redux Toolkit.'
-                    github='https://github.com/AnnaTas77/online-store-project'
+                    description="A simple weather app created with JavaScript, CSS and HTML. The app returns current weather data from the Open Weather Map API via Fetch API."
+                    live="https://annatas-weather-app.netlify.app/"
+                    github={["https://github.com/AnnaTas77/weather-app"]}
                 />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Projects
+export default Projects;
