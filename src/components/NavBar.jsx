0b8872459e8cab727dd1571057fc91905e38d325
Anna Tasheva <anna.tasheva90@gmail.com>
2023-08-23 09:59:43 +0100

    Refactored the NavBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,61 +1,60 @@
-import React, { useEffect, useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
-import Logo from "../assets/At_neon2.png";
-import "../styles/navbar.css";
+import { BiHomeAlt, BiUser } from "react-icons/bi";
+import { BsChatSquareText } from "react-icons/bs";
+import { FaLaptopCode } from "react-icons/fa";
+import { HiOutlineBadgeCheck } from "react-icons/hi";
 import { Link } from "react-scroll";
+import "../index.css";
 
 const NavBar = () => {
-    const [expandNavbar, setExpandNavbar] = useState(false);
-
-    const handleClick = () => setExpandNavbar(!expandNavbar);
-
-    const [small, setSmall] = useState(false);
-
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            window.addEventListener("scroll", () => {
-                setSmall(window.pageYOffset > 200);
-            });
-        }
-    }, []);
-
     return (
-        <nav className={`navbar-container ${small ? "navbar-small" : ""}`} id={expandNavbar ? "open" : "close"}>
-            <h1 className={`logo-initials ${small ? "logo-initials-small" : ""}`}>AT</h1>
-
-            <div className="navbar-links">
-                <ul>
-                    <li>
-                        <Link to="about" smooth={true} duration={500}>
-                            About
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="projects" smooth={true} duration={500}>
-                            Projects
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="contact" smooth={true} duration={500}>
-                            Contact
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-
-            <div className="hamburger-button">
-                <button onClick={handleClick}>{!expandNavbar ? <FaBars /> : <FaTimes />}</button>
-            </div>
-
-            <div className="mobile-menu" onClick={handleClick}>
-                <Link to="home" smooth={true} duration={500}>
-                    Home
+        <nav className="flex justify-center items-center mt-6 w-full fixed bottom-4 lg:bottom-8 z-[100]">
+            <div className="w-[80%] bg-black/20 h-[76px] lg:h-[86px] backdrop-blur-3xl rounded-full max-w-[460px] px-5 flex justify-between items-center align-middle text-2xl text-white/70">
+                <Link
+                    to="home"
+                    smooth={true}
+                    spy={true}
+                    offset={-200}
+                    activeClass="active"
+                    className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+                >
+                    <BiHomeAlt className="font-bold" />
                 </Link>
-                <Link to="projects" smooth={true} duration={500}>
-                    Projects
+                <Link
+                    to="about"
+                    smooth={true}
+                    spy={true}
+                    activeClass="active"
+                    className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+                >
+                    <BiUser className="font-bold" />
                 </Link>
-                <Link to="contact" smooth={true} duration={500}>
-                    Contact
+                <Link
+                    to="skills"
+                    smooth={true}
+                    spy={true}
+                    activeClass="active"
+                    className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+                >
+                    <HiOutlineBadgeCheck className="font-bold" />
+                </Link>
+                <Link
+                    to="projects"
+                    smooth={true}
+                    spy={true}
+                    activeClass="active"
+                    className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+                >
+                    <FaLaptopCode className="font-bold" />
+                </Link>
+
+                <Link
+                    to="contact"
+                    smooth={true}
+                    spy={true}
+                    activeClass="active"
+                    className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+                >
+                    <BsChatSquareText className="font-bold" />
                 </Link>
             </div>
         </nav>
