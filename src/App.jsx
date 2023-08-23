d6a5e5c30505b9b1825d98189235f9af5b2000c7
Anna Tasheva <anna.tasheva90@gmail.com>
2023-08-23 10:51:26 +0100

    Refactored app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import "./index.css";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import Skills from "./components/Skills";
 
 function App() {
     return (
-        <div className="App">
+        <div className="bg-[url('./assets/site-bg.jpg')] bg-no-repeat bg-cover overflow-hidden text-slate-200 min-h-screen flex flex-col items-center justify-between">
+            <Header />
             <NavBar />
-            <main>
+            <main className="flex flex-col items-center justify-center w-[92%]">
+                <Home />
                 <About />
+                <Skills />
                 <Projects />
                 <Contact />
             </main>
