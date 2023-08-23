9e1f0021a19de288663406b2006fd324e8a3cd90
Anna Tasheva <anna.tasheva90@gmail.com>
2023-08-23 10:01:36 +0100

    Refactored the Project component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,51 +1,33 @@
-import React from "react";
-import "../styles/projects.css";
-import ProjectItem from "./ProjectItem";
-import RedditGeeks from "../assets/RedditScreenshot7.jpg";
-import WeatherApp from "../assets/WeatherApp_merged7.jpg";
-import NcNews from "../assets/NcNews.png";
-import Meteorites from "../assets/Meteorite Landings Viewer.png";
+import ProjectCard from "./ProjectCard";
+
+import { projects } from "../data/data.js";
 
 const Projects = () => {
     return (
-        <div className="projects" id="projects">
-            <h1 className="title">Personal Projects</h1>
-            <div className="project-list">
-                <ProjectItem
-                    name="NC News"
-                    image={NcNews}
-                    description="NC News is a full-stack project with a custom backend News API, mimicking Reddit's functionalities. The frontend serves as a user interface, enabling access to various API endpoints for seamless interaction with news content."
-                    live="https://nc-news-anna.netlify.app/"
-                    github={[
-                        { label: "Frontend Code", url: "https://github.com/AnnaTas77/nc-news-frontend-project" },
-                        { label: "Backend Code", url: "https://github.com/AnnaTas77/nc-news-backend-project" },
-                    ]}
-                />
+        <section
+            className="flex flex-col w-full items-center justify-center min-h-screen pt-20 mb-[100px]"
+            id="projects"
+        >
+            <h2 className="text-center w-full text-xl py-5 mb-10 mt-[30px] font-bold md:text-2xl lg:text-3xl bg-gradient-to-r text-transparent bg-clip-text from-[#42A6E3] to-[#FF56F6]">
+                Portfolio Projects
+            </h2>
 
-                <ProjectItem
-                    name="Meteorite Landings Interactive Map"
-                    image={Meteorites}
-                    description="This project features an interactive map of meteorite landings, sourcing data from NASA's Meteorite Landings API. Users can conveniently sort the table of all known landings by name, year of discovery, and mass in grams by clicking the column headings."
-                    github={["https://github.com/AnnaTas77/fe-react-data-visualisation"]}
-                />
-
-                <ProjectItem
-                    name="Reddit Geeks"
-                    image={RedditGeeks}
-                    description="Reddit Geeks is a portfolio project for a subreddit feed app which can search through subreddits and display their articles."
-                    live="https://annatas77.github.io/redditgeeks/"
-                    github={["https://github.com/AnnaTas77/redditgeeks"]}
-                />
-
-                <ProjectItem
-                    name="Weather App"
-                    image={WeatherApp}
-                    description="A simple weather app created with JavaScript, CSS and HTML. The app returns current weather data from the Open Weather Map API via Fetch API."
-                    live="https://annatas-weather-app.netlify.app/"
-                    github={["https://github.com/AnnaTas77/weather-app"]}
-                />
+            <div className="flex flex-col lg:flex-row lg:flex-wrap items-center justify-center w-full gap-8">
+                {projects.map((project) => {
+                    return (
+                        <ProjectCard
+                            key={project.title}
+                            title={project.title}
+                            description={project.description}
+                            image={project.image}
+                            tech={project.tech}
+                            live={project.live}
+                            github={project.github}
+                        />
+                    );
+                })}
             </div>
-        </div>
+        </section>
     );
 };
 
