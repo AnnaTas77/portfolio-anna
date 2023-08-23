f9689888fe162aa2ee848d1c0ca3e87524289cd2
Anna Tasheva <anna.tasheva90@gmail.com>
2023-08-23 09:55:54 +0100

    Refactored the Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import "../styles/footer.css";
-
 const Footer = () => {
     return (
-        <div className="footer">
-            <p>&copy; {new Date().getFullYear()} Anna Tasheva</p>
-        </div>
+        <footer className="flex items-center justify-center w-full text-slate-200 p-10">
+            <p>
+                Anna Tasheva <span>{new Date().getFullYear()}</span>
+            </p>
+        </footer>
     );
 };
 
